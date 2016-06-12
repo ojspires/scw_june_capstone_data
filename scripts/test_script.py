46b235811272bf145d46f7a813637dc23d80623a
Oliver Spires <ojspires@vm64-31.iplantcollaborative.org>
2016-06-12 15:54:12 -0700

    edited script

---
+++ scripts/test_script.py
@@ -0,0 +1,9 @@
+'''
+Jake Garan, Oliver Spires
+Software Carpentry Workshop Capstone Project
+'''
+import pandas as pd
+from matplotlib import pyplot as plt
+%matplotlib inline
+df = pd.read_csv('../data/gapminder*')
+
